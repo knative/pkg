2dbc2f9b7f4d77f6f907a0b2f14ce3b30f895157
Scott Andrews <sandrews@pivotal.io>
2018-08-03 11:02:21 -0400

    nix linguring serving package dep

--- apis/istio/v1alpha3/register.go
+++ apis/istio/v1alpha3/register.go
@@ -17,7 +17,7 @@ limitations under the License.
 package v1alpha3
 
 import (
-	"github.com/knative/serving/pkg/apis/istio"
+	"github.com/knative/pkg/apis/istio"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
