77968d942012235c80216f08305851b92f313418
Markus Thömmes <markusthoemmes@me.com>
2018-10-10 09:17:14 +0200

    Add timeKey so time is actually logged in e2e tests.

--- test/logging/logging.go
+++ test/logging/logging.go
@@ -77,17 +77,18 @@ func newLogger(logLevel string) *BaseLogger {
 	  "outputPaths": ["stdout"],
 	  "errorOutputPaths": ["stderr"],
 	  "encoderConfig": {
+	    "timeKey": "ts",
 	    "messageKey": "message",
-			"levelKey": "level",
-			"nameKey": "logger",
-			"callerKey": "caller",
-			"messageKey": "msg",
-      "stacktraceKey": "stacktrace",
-      "lineEnding": "",
-      "levelEncoder": "",
-      "timeEncoder": "iso8601",
-      "durationEncoder": "",
-      "callerEncoder": ""
+	    "levelKey": "level",
+	    "nameKey": "logger",
+	    "callerKey": "caller",
+	    "messageKey": "msg",
+	    "stacktraceKey": "stacktrace",
+	    "lineEnding": "",
+	    "levelEncoder": "",
+	    "timeEncoder": "",
+	    "durationEncoder": "iso8601",
+	    "callerEncoder": ""
 	  }
 	}`
 	configJSON := fmt.Sprintf(configJSONTemplate, logLevel)
