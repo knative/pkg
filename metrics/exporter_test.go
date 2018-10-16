7ed9ac61681a662de633f94ac9214723e8e256bf
Yao Wu <wuyao@google.com>
2018-10-16 10:46:25 -0700

    add a test case for stackdriver error check

--- metrics/exporter_test.go
+++ metrics/exporter_test.go
@@ -33,6 +33,15 @@ func TestNewStackdriverExporter(t *testing.T) {
 		t.Error("expected a non-nil metrics exporter")
 	}
 	expectNoPromSrv(t)
+
+	e, err = newStackdriverExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Stackdriver,
+		stackdriverProjectID: ""}, TestLogger(t))
+	if err == nil {
+		t.Error("Expected an error when stackdriver project ID is empty")
+	}
 }
 
 func TestNewPrometheusExporter(t *testing.T) {
