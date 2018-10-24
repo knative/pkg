10ceeb6163ab85da29d2625f8749352e53dd5f7c
akyyy <35933136+akyyy@users.noreply.github.com>
2018-10-24 15:23:32 -0700

    Add stackdriver exporter support in pkg. (#126)
    * update dep golang.org/x/net
    
    * Add stackdriver exporter support in pkg
    
    * fix race and tests
    
    * Use thread safe newMetricsExporter in tests
    
    * address cr comments
    
    * minor fix
    
    * checkpoint
    
    * Address cr comments
    
    * add test for UpdateExporterFromConfigMap
    
    * Move lock position
    
    * add a test case for stackdriver error check
    
    * minor change
    
    * removing one test case since it seems app default creds exists for presubmit tests
    
    * replace promSrvChan with promSrv as cr suggsted
    
    * use a local var server for ListenAndServe
    
    * add getter and setter for shared variables
    
    * add func isMetricsConfigChanged and test
    
    * address cr comments
    
    * Update opencensus-go-exporter-stackdriver to v0.7.0
    
    * Revert "Update opencensus-go-exporter-stackdriver to v0.7.0"
    
    This reverts commit 9825f641f885f420c8e162357e2ef315165ca02d.
    
    * run update-deps.sh
    
    * merge conflict
    
    * run update-deps.sh
    
    * Update Gopkg.toml so it doesn't have stackdriver exporter
    
    * remove regex for metrics domain at pkg repo per cr comments
    
    * merge conflict

---
+++ metrics/exporter_test.go
@@ -0,0 +1,121 @@
+/*
+Copyright 2018 The Knative Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package metrics
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/knative/pkg/logging/testing"
+)
+
+func TestNewStackdriverExporter(t *testing.T) {
+	// The stackdriver project ID is required for stackdriver exporter.
+	e, err := newStackdriverExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Stackdriver,
+		stackdriverProjectID: testProj}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+	if e == nil {
+		t.Error("expected a non-nil metrics exporter")
+	}
+	expectNoPromSrv(t)
+}
+
+func TestNewPrometheusExporter(t *testing.T) {
+	// The stackdriver project ID is not required for prometheus exporter.
+	e, err := newPrometheusExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Prometheus,
+		stackdriverProjectID: ""}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+	if e == nil {
+		t.Error("expected a non-nil metrics exporter")
+	}
+	expectPromSrv(t)
+}
+
+func TestMetricsExporter(t *testing.T) {
+	err := newMetricsExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   "unsupported",
+		stackdriverProjectID: ""}, TestLogger(t))
+	if err == nil {
+		t.Errorf("Expected an error for unsupported backend %v", err)
+	}
+
+	err = newMetricsExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Stackdriver,
+		stackdriverProjectID: testProj}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInterlevedExporters(t *testing.T) {
+	// First create a stackdriver exporter
+	err := newMetricsExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Stackdriver,
+		stackdriverProjectID: testProj}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+	expectNoPromSrv(t)
+	// Then switch to prometheus exporter
+	err = newMetricsExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Prometheus,
+		stackdriverProjectID: "",
+	}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+	expectPromSrv(t)
+	// Finally switch to stackdriver exporter
+	err = newMetricsExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            testComponent,
+		backendDestination:   Stackdriver,
+		stackdriverProjectID: testProj}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func expectPromSrv(t *testing.T) {
+	time.Sleep(200 * time.Millisecond)
+	srv := getCurPromSrv()
+	if srv == nil {
+		t.Error("expected a server for prometheus exporter")
+	}
+}
+
+func expectNoPromSrv(t *testing.T) {
+	time.Sleep(200 * time.Millisecond)
+	srv := getCurPromSrv()
+	if srv != nil {
+		t.Error("expected no server for stackdriver exporter")
+	}
+}
