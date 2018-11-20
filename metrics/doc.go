b2f0ffd9adbf433764222bd978b995a1b92c4be4
Yao Wu <wuyao@google.com>
2018-11-20 14:16:35 -0800

    Update comment

--- metrics/doc.go
+++ metrics/doc.go
@@ -11,8 +11,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-Package metrics provides utilities for exporting metrics to Stackdriver backend
-or Prometheus backend based on config-observability settings.
-*/
+// Package metrics provides Knative utilities for exporting metrics to Stackdriver
+// backend or Prometheus backend based on config-observability settings.
 package metrics
