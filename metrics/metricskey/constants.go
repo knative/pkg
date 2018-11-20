0a2c5960f02f63d90eb113b37415a9063a7d0f74
Yao Wu <wuyao@google.com>
2018-11-20 13:46:30 -0800

    address cr comments

--- metrics/metricskey/constants.go
+++ metrics/metricskey/constants.go
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package metricsKey
+package metricskey
 
 const (
 	// ResourceTypeKnativeRevision is the Stackdriver resource type for Knative revision
