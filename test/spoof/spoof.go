f6233919a4a8f5b77b74f1718776721307dbb0da
Matt Moore (via sockpuppet) <mattmoor+sockpuppet@google.com>
2018-11-30 16:17:50 +0000

    Run goimports
    Produced via: `goimports -w $(find -name '*.go' | grep -v vendor)`

--- test/spoof/spoof.go
+++ test/spoof/spoof.go
@@ -30,7 +30,6 @@ import (
 
 	"github.com/knative/pkg/test/logging"
 	zipkin "github.com/knative/pkg/test/zipkin"
-	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
