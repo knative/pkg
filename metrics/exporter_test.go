08febf8f13246f4217581904dcfb25ffebf52814
Yanwei Guo <yanweiguo@google.com>
2019-01-08 13:14:42 -0800

    Change Stackdriver resource type for metrics (#210)
    * change resource type
    
    * copied PR #188
    
    * add unit tests
    
    * change Fatal back to Error
    
    * solve comments
    
    * change to use map
    
    * modify tests

--- metrics/exporter_test.go
+++ metrics/exporter_test.go
@@ -13,18 +13,65 @@ limitations under the License.
 package metrics
 
 import (
+	"os"
 	"testing"
 	"time"
 
 	. "github.com/knative/pkg/logging/testing"
+	"github.com/knative/pkg/metrics/metricskey"
+	"go.opencensus.io/stats"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 )
+
+const (
+	testNS            = "test"
+	testService       = "test-service"
+	testRoute         = "test-route"
+	testConfiguration = "test-configuration"
+	testRevision      = "test-revision"
+)
+
+var (
+	testView = &view.View{
+		Description: "Test View",
+		Measure:     stats.Int64("test", "Test Measure", stats.UnitNone),
+		Aggregation: view.LastValue(),
+		TagKeys:     []tag.Key{},
+	}
+
+	nsKey       = tag.Tag{Key: mustNewTagKey(metricskey.LabelNamespaceName), Value: testNS}
+	serviceKey  = tag.Tag{Key: mustNewTagKey(metricskey.LabelServiceName), Value: testService}
+	routeKey    = tag.Tag{Key: mustNewTagKey(metricskey.LabelRouteName), Value: testRoute}
+	revisionKey = tag.Tag{Key: mustNewTagKey(metricskey.LabelRevisionName), Value: testRevision}
+
+	testTags = []tag.Tag{nsKey, serviceKey, routeKey, revisionKey}
+)
+
+func mustNewTagKey(s string) tag.Key {
+	tagKey, err := tag.NewKey(s)
+	if err != nil {
+		panic(err)
+	}
+	return tagKey
+}
+
+func getResourceLabelValue(key string, tags []tag.Tag) string {
+	for _, t := range tags {
+		if t.Key.Name() == key {
+			return t.Value
+		}
+	}
+	return ""
+}
 
 func TestMain(m *testing.M) {
 	resetCurPromSrv()
-	m.Run()
+	os.Exit(m.Run())
 }
 
-func TestNewStackdriverExporter(t *testing.T) {
+func TestNewStackdriverExporterForGlobal(t *testing.T) {
+	resetMonitoredResourceFunc()
 	// The stackdriver project ID is required for stackdriver exporter.
 	e, err := newStackdriverExporter(&metricsConfig{
 		domain:               servingDomain,
@@ -36,6 +83,58 @@ func TestNewStackdriverExporter(t *testing.T) {
 	}
 	if e == nil {
 		t.Error("expected a non-nil metrics exporter")
+	}
+	if getMonitoredResourceFunc == nil {
+		t.Error("expected a non-nil getMonitoredResourceFunc")
+	}
+	newTags, monitoredResource := getMonitoredResourceFunc(testView, testTags)
+	gotResType, labels := monitoredResource.MonitoredResource()
+	wantedResType := "global"
+	if gotResType != wantedResType {
+		t.Errorf("MonitoredResource=%v, got: %v", wantedResType, gotResType)
+	}
+	got := getResourceLabelValue(metricskey.LabelNamespaceName, newTags)
+	if got != testNS {
+		t.Errorf("expected new tag %v with value %v, got: %v", routeKey, testNS, newTags)
+	}
+	if len(labels) != 0 {
+		t.Errorf("expected no label, got: %v", labels)
+	}
+}
+
+func TestNewStackdriverExporterForKnativeRevision(t *testing.T) {
+	resetMonitoredResourceFunc()
+	e, err := newStackdriverExporter(&metricsConfig{
+		domain:               servingDomain,
+		component:            "autoscaler",
+		backendDestination:   Stackdriver,
+		stackdriverProjectID: testProj}, TestLogger(t))
+	if err != nil {
+		t.Error(err)
+	}
+	if e == nil {
+		t.Error("expected a non-nil metrics exporter")
+	}
+	if getMonitoredResourceFunc == nil {
+		t.Error("expected a non-nil getMonitoredResourceFunc")
+	}
+	newTags, monitoredResource := getMonitoredResourceFunc(testView, testTags)
+	gotResType, labels := monitoredResource.MonitoredResource()
+	wantedResType := "knative_revision"
+	if gotResType != wantedResType {
+		t.Errorf("MonitoredResource=%v, got %v", wantedResType, gotResType)
+	}
+	got := getResourceLabelValue(metricskey.LabelRouteName, newTags)
+	if got != testRoute {
+		t.Errorf("expected new tag: %v, got: %v", routeKey, newTags)
+	}
+	got, ok := labels[metricskey.LabelNamespaceName]
+	if !ok || got != testNS {
+		t.Errorf("expected label %v with value %v, got: %v", metricskey.LabelNamespaceName, testNS, got)
+	}
+	got, ok = labels[metricskey.LabelConfigurationName]
+	if !ok || got != metricskey.ValueUnknown {
+		t.Errorf("expected label %v with value %v, got: %v", metricskey.LabelConfigurationName, metricskey.ValueUnknown, got)
 	}
 }
 
@@ -91,8 +190,7 @@ func TestInterlevedExporters(t *testing.T) {
 		domain:               servingDomain,
 		component:            testComponent,
 		backendDestination:   Prometheus,
-		stackdriverProjectID: "",
-	}, TestLogger(t))
+		stackdriverProjectID: ""}, TestLogger(t))
 	if err != nil {
 		t.Error(err)
 	}
