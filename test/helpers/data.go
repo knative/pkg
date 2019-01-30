78d77df6e233c5ba468296f8a8610380421f44cb
Leonardo Rossetti <me@lrossetti.com>
2019-01-30 04:51:27 +0000

    test/helpers module to store AppendRandomString helper fn

---
+++ test/helpers/data.go
@@ -0,0 +1,39 @@
+package helpers
+
+import (
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyz"
+	randSuffixLen = 8
+	sep = "-"
+)
+
+var (
+	r        *rand.Rand
+	rndMutex *sync.Mutex
+	once     sync.Once
+)
+
+func initSeed() {
+	seed := time.Now().UTC().UnixNano()
+	r = rand.New(rand.NewSource(seed))
+	rndMutex = &sync.Mutex{}
+}
+
+func AppendRandomString(prefix string) string {
+	once.Do(initSeed)
+	suffix := make([]byte, randSuffixLen)
+
+	rndMutex.Lock()
+	for i := range suffix {
+		suffix[i] = letterBytes[r.Intn(len(letterBytes))]
+	}
+	rndMutex.Unlock()
+
+	return strings.Join([]string{prefix, string(suffix)}, sep)
+}
