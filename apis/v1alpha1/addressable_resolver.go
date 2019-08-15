4189195a599f7baac71aac57a74453bc6403a0e6
Spencer Peterson <spencerjp@google.com>
2019-08-15 20:35:43 +0000

    addressable resolver and tests

---
+++ apis/v1alpha1/addressable_resolver.go
@@ -0,0 +1,133 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+	"path"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	"knative.dev/eventing/pkg/reconciler/names"
+	"knative.dev/pkg/apis"
+	pkgapisduck "knative.dev/pkg/apis/duck"
+	duckv1alpha1 "knative.dev/pkg/apis/duck/v1alpha1"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/tracker"
+
+	"knative.dev/pkg/injection/clients/dynamicclient"
+)
+
+// AddressableResolver resolves a Destination into a URI.
+// This is a helper component for resources that references other resources' addresses.
+type AddressableResolver struct {
+	tracker         tracker.Interface
+	informerFactory pkgapisduck.InformerFactory
+}
+
+// NewAddressableResolver creates and initializes a new AddressableResolver.
+func NewAddressableResolver(ctx context.Context, callback func(string)) *AddressableResolver {
+	ret := &AddressableResolver{}
+
+	ret.tracker = tracker.New(callback, controller.GetTrackerLease(ctx))
+	ret.informerFactory = &pkgapisduck.CachedInformerFactory{
+		Delegate: &pkgapisduck.EnqueueInformerFactory{
+			Delegate: &pkgapisduck.TypedInformerFactory{
+				Client:       dynamicclient.Get(ctx),
+				Type:         &duckv1alpha1.AddressableType{},
+				ResyncPeriod: controller.GetResyncPeriod(ctx),
+				StopChannel:  ctx.Done(),
+			},
+			EventHandler: controller.HandleAll(ret.tracker.OnChanged),
+		},
+	}
+
+	return ret
+}
+
+// GetURI resolves a Destination into a URI string.
+func (r *AddressableResolver) GetURI(dest Destination, parent interface{}) (string, error) {
+	// sinkObjRef *corev1.ObjectReference, source interface{}, sourceDesc string
+	if dest.ObjectReference != nil {
+		url, err := r.resolveObjectReference(dest.ObjectReference, parent)
+		if err != nil {
+			return "", err
+		}
+		return extendPath(url, dest.Path).String(), nil
+	} else if dest.URI != nil {
+		return extendPath(dest.URI, dest.Path).String(), nil
+	} else {
+		return "", fmt.Errorf("destination missing ObjectReference and URI, expected at least one")
+	}
+}
+
+func (r *AddressableResolver) resolveObjectReference(ref *corev1.ObjectReference, parent interface{}) (*apis.URL, error) {
+	if ref == nil {
+		return nil, fmt.Errorf("ref is nil")
+	}
+
+	if err := r.tracker.Track(*ref, parent); err != nil {
+		return nil, fmt.Errorf("failed to track %+v: %+v", ref, err)
+	}
+
+	// K8s Services are special cased. They can be called, even though they do not satisfy the
+	// Callable interface.
+	if ref.APIVersion == "v1" && ref.Kind == "Service" {
+		url := &apis.URL{
+			Scheme: "http",
+			Host:   names.ServiceHostName(ref.Name, ref.Namespace),
+			Path:   "/",
+		}
+		return url, nil
+	}
+
+	gvr, _ := meta.UnsafeGuessKindToResource(ref.GroupVersionKind())
+	_, lister, err := r.informerFactory.Get(gvr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get lister for %+v: %+v", gvr, err)
+	}
+
+	obj, err := lister.ByNamespace(ref.Namespace).Get(ref.Name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get ref %+v: %+v", ref, err)
+	}
+
+	addressable, ok := obj.(*duckv1alpha1.AddressableType)
+	if !ok {
+		return nil, fmt.Errorf("%+v is not an AddressableType", ref)
+	}
+	if addressable.Status.Address == nil {
+		return nil, fmt.Errorf("address not set for %+v", ref)
+	}
+	url := addressable.Status.Address.GetURL()
+	if url.Host == "" {
+		return nil, fmt.Errorf("missing hostname in address of %+v", ref)
+	}
+	return &url, nil
+}
+
+// extendPath is a convenience wrapper to add a destination's path.
+func extendPath(url *apis.URL, extrapath *string) *apis.URL {
+	if extrapath == nil {
+		return url
+	}
+
+	url.Path = path.Join(url.Path, *extrapath)
+	return url
+}
---
+++ apis/v1alpha1/addressable_resolver_test.go
@@ -0,0 +1,255 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"knative.dev/pkg/apis"
+	duckv1alpha1 "knative.dev/pkg/apis/duck/v1alpha1"
+	fakedynamicclient "knative.dev/pkg/injection/clients/dynamicclient/fake"
+	"knative.dev/pkg/ptr"
+)
+
+var (
+	addressableDNS = "addressable.sink.svc.cluster.local"
+
+	addressableName       = "testsink"
+	addressableKind       = "Sink"
+	addressableAPIVersion = "duck.knative.dev/v1alpha1"
+
+	unaddressableName       = "testunaddressable"
+	unaddressableKind       = "KResource"
+	unaddressableAPIVersion = "duck.knative.dev/v1alpha1"
+	unaddressableResource   = "kresources.duck.knative.dev"
+
+	testNS = "testnamespace"
+)
+
+func init() {
+	// Add types to scheme
+	duckv1alpha1.AddToScheme(scheme.Scheme)
+}
+
+func TestGetURI_ObjectReference(t *testing.T) {
+	tests := map[string]struct {
+		objects []runtime.Object
+		dest    Destination
+		wantURI string
+		wantErr error
+	}{"nil everything": {
+		wantErr: fmt.Errorf("destination missing ObjectReference and URI, expected at least one"),
+	}, "URI with path": {
+		dest: Destination{
+			URI: &apis.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/foo",
+			},
+			Path: ptr.String("/bar"),
+		},
+		wantURI: "http://example.com/foo/bar",
+	}, "URI with path without leading slash": {
+		dest: Destination{
+			URI: &apis.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/foo",
+			},
+			Path: ptr.String("bar"),
+		},
+		wantURI: "http://example.com/foo/bar",
+	}, "URI with garbage path": {
+		dest: Destination{
+			URI: &apis.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/foo",
+			},
+			Path: ptr.String("////bar///"),
+		},
+		wantURI: "http://example.com/foo/bar",
+	}, "URI with nil path": {
+		dest: Destination{
+			URI: &apis.URL{
+				Scheme: "http",
+				Host:   "example.com",
+				Path:   "/foo",
+			},
+			Path: nil,
+		},
+		wantURI: "http://example.com/foo",
+	}, "happy objectref": {
+		objects: []runtime.Object{
+			getAddressable(),
+		},
+		dest:    Destination{ObjectReference: getAddressableRef()},
+		wantURI: fmt.Sprintf("http://%s", addressableDNS),
+	}, "object ref with path": {
+		objects: []runtime.Object{
+			getAddressable(),
+		},
+		dest: Destination{
+			ObjectReference: getAddressableRef(),
+			Path:            ptr.String("/foo"),
+		},
+		wantURI: fmt.Sprintf("http://%s/foo", addressableDNS),
+	}, "object ref with path without leading slash": {
+		objects: []runtime.Object{
+			getAddressable(),
+		},
+		dest: Destination{
+			ObjectReference: getAddressableRef(),
+			Path:            ptr.String("foo"),
+		},
+		wantURI: fmt.Sprintf("http://%s/foo", addressableDNS),
+	}, "nil hostname": {
+		objects: []runtime.Object{
+			getAddressableNilHostname(),
+		},
+		dest:    Destination{ObjectReference: getUnaddressableRef()},
+		wantErr: fmt.Errorf(`missing hostname in address of %+v`, getUnaddressableRef()),
+	}, "nil address": {
+		objects: []runtime.Object{
+			getAddressableNilAddress(),
+		},
+		dest:    Destination{ObjectReference: getUnaddressableRef()},
+		wantErr: fmt.Errorf(`address not set for %+v`, getUnaddressableRef()),
+	}, "missing status": {
+		objects: []runtime.Object{
+			getAddressableNoStatus(),
+		},
+		dest:    Destination{ObjectReference: getUnaddressableRef()},
+		wantErr: fmt.Errorf(`address not set for %+v`, getUnaddressableRef()),
+	}, "notFound": {
+		dest:    Destination{ObjectReference: getUnaddressableRef()},
+		wantErr: fmt.Errorf(`failed to get ref %+v: %s "%s" not found`, getUnaddressableRef(), unaddressableResource, unaddressableName),
+	}}
+
+	for n, tc := range tests {
+		t.Run(n, func(t *testing.T) {
+			ctx, _ := fakedynamicclient.With(context.Background(), scheme.Scheme, tc.objects...)
+			r := NewAddressableResolver(ctx, func(string) {})
+			uri, gotErr := r.GetURI(tc.dest, getAddressable())
+			if gotErr != nil {
+				if tc.wantErr != nil {
+					if diff := cmp.Diff(tc.wantErr.Error(), gotErr.Error()); diff != "" {
+						t.Errorf("%s: unexpected error (-want, +got) = %v", n, diff)
+					}
+				} else {
+					t.Errorf("%s: unexpected error: %v", n, gotErr.Error())
+				}
+			}
+			if gotErr == nil {
+				got := uri
+				if diff := cmp.Diff(tc.wantURI, got); diff != "" {
+					t.Errorf("%s: unexpected object (-want, +got) = %v", n, diff)
+				}
+			}
+		})
+	}
+}
+
+func getAddressable() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": addressableAPIVersion,
+			"kind":       addressableKind,
+			"metadata": map[string]interface{}{
+				"namespace": testNS,
+				"name":      addressableName,
+			},
+			"status": map[string]interface{}{
+				"address": map[string]interface{}{
+					"hostname": addressableDNS,
+				},
+			},
+		},
+	}
+}
+
+func getAddressableNoStatus() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": unaddressableAPIVersion,
+			"kind":       unaddressableKind,
+			"metadata": map[string]interface{}{
+				"namespace": testNS,
+				"name":      unaddressableName,
+			},
+		},
+	}
+}
+
+func getAddressableNilAddress() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": unaddressableAPIVersion,
+			"kind":       unaddressableKind,
+			"metadata": map[string]interface{}{
+				"namespace": testNS,
+				"name":      unaddressableName,
+			},
+			"status": map[string]interface{}{
+				"address": map[string]interface{}(nil),
+			},
+		},
+	}
+}
+
+func getAddressableNilHostname() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": unaddressableAPIVersion,
+			"kind":       unaddressableKind,
+			"metadata": map[string]interface{}{
+				"namespace": testNS,
+				"name":      unaddressableName,
+			},
+			"status": map[string]interface{}{
+				"address": map[string]interface{}{
+					"hostname": nil,
+				},
+			},
+		},
+	}
+}
+
+func getAddressableRef() *corev1.ObjectReference {
+	return &corev1.ObjectReference{
+		Kind:       addressableKind,
+		Name:       addressableName,
+		APIVersion: addressableAPIVersion,
+		Namespace:  testNS,
+	}
+}
+
+func getUnaddressableRef() *corev1.ObjectReference {
+	return &corev1.ObjectReference{
+		Kind:       unaddressableKind,
+		Name:       unaddressableName,
+		APIVersion: unaddressableAPIVersion,
+		Namespace:  testNS,
+	}
+}
