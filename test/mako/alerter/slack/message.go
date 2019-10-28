dd50e7a68e4d16bb4ad0c6b1936e7831e691c9d6
Chi Zhang <chizhg@google.com>
2019-10-28 15:08:46 -0700

    address comments

--- test/mako/alerter/slack/message.go
+++ test/mako/alerter/slack/message.go
@@ -32,7 +32,7 @@ var minInterval = flag.Duration("min-alert-interval", 24*time.Hour, "The minimum
 
 const (
 	messageTemplate = `
-As of %s, there is a new performance regression detected from test automation for *%s*:
+As of %s, there is a new performance regression detected from test automation for %s:
 %s`
 )
 
@@ -86,16 +86,18 @@ func (smh *MessageHandler) SendAlert(testName, summary string) error {
 			); err != nil {
 				errCh <- fmt.Errorf("failed to retrieve message history in channel %q", channel.Name)
 			}
+			// decorate the test name for more accurate match
+			decoratedTestName := decoratedName(testName)
 			// do not send message again if alert for this test has been sent to
 			// the channel a short while ago
 			for _, message := range messageHistory {
-				if strings.Contains(message, testName) {
+				if strings.Contains(message, decoratedTestName) {
 					return
 				}
 			}
 
 			// send the alert message to the channel
-			message := fmt.Sprintf(messageTemplate, time.Now().UTC(), testName, summary)
+			message := fmt.Sprintf(messageTemplate, time.Now().UTC(), decoratedTestName, summary)
 			if err := helpers.Run(
 				fmt.Sprintf("sending message %q to channel %q", message, channel.Name),
 				func() error {
@@ -119,4 +121,9 @@ func (smh *MessageHandler) SendAlert(testName, summary string) error {
 	}
 
 	return helpers.CombineErrors(errs)
+}
+
+// decoratedName returns a name with decoration for easy identification.
+func decoratedName(name string) string {
+	return fmt.Sprintf("[%s]", name)
 }
