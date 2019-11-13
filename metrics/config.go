4deb5d83d26170faeef8e54e9ae4cd9b04ed81f8
Nima Kaviani <nkavian@us.ibm.com>
2019-11-13 06:52:28 -0800

    move metrics config to pkg (#786)
    the observability config is used in other packages including
    the sample controller. This commit pulls observability config
    into knative.dev/pkg similar to logging config for better reusability.

--- metrics/config.go
+++ metrics/config.go
@@ -33,8 +33,7 @@ import (
 )
 
 const (
-	DomainEnv        = "METRICS_DOMAIN"
-	ConfigMapNameEnv = "CONFIG_OBSERVABILITY_NAME"
+	DomainEnv = "METRICS_DOMAIN"
 )
 
 // metricsBackend specifies the backend to use for metrics
@@ -246,15 +245,6 @@ func createMetricsConfig(ops ExporterOptions, logger *zap.SugaredLogger) (*metri
 	}
 
 	return &mc, nil
-}
-
-// ConfigMapName gets the name of the metrics ConfigMap
-func ConfigMapName() string {
-	cm := os.Getenv(ConfigMapNameEnv)
-	if cm == "" {
-		return "config-observability"
-	}
-	return cm
 }
 
 // Domain holds the metrics domain to use for surfacing metrics.
