e12b3a5491a80d0fe30cd6d0e15cb963869644af
Scott Nichols <snichols@vmware.com>
2020-01-07 14:44:17 -0800

    cleanup test code.

--- reconciler/events_test.go
+++ reconciler/events_test.go
@@ -32,42 +32,42 @@ const (
 func TestNil_Is(t *testing.T) {
 	var err error
 	if EventIs(err, NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "")) {
-		t.Error("did not expect error to be a ReconcilerEvent")
+		t.Error("Did not expect error to be a ReconcilerEvent")
 	}
 }
 
 func TestError_Is(t *testing.T) {
 	err := errors.New("some other error")
 	if EventIs(err, NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "")) {
-		t.Error("did not expect error to be a ReconcilerEvent")
+		t.Error("Did not expect error to be a ReconcilerEvent")
 	}
 }
 
 func TestNew_Is(t *testing.T) {
 	err := NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "this is an example error, %s", "yep")
 	if !EventIs(err, NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "")) {
-		t.Error("expected error to be a [Warn, ExampleStatusFailed]")
+		t.Error("Expected error to be a [Warn, ExampleStatusFailed]")
 	}
 }
 
 func TestNewOtherType_Is(t *testing.T) {
 	err := NewEvent(corev1.EventTypeNormal, exampleStatusFailed, "this is an example error, %s", "yep")
 	if EventIs(err, NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "")) {
-		t.Error("expected error to be a [Normal, ExampleStatusFailed], filtered by eventtype failed")
+		t.Error("Expected error to be a [Normal, ExampleStatusFailed], filtered by eventtype failed")
 	}
 }
 
 func TestNewWrappedErrors_Is(t *testing.T) {
 	err := NewEvent(corev1.EventTypeNormal, exampleStatusFailed, "this is a wrapped error, %w", io.ErrUnexpectedEOF)
 	if !EventIs(err, io.ErrUnexpectedEOF) {
-		t.Error("event expected to be a wrapped ErrUnexpectedEOF but was not")
+		t.Error("Event expected to be a wrapped ErrUnexpectedEOF but was not")
 	}
 }
 
 func TestNewOtherReason_Is(t *testing.T) {
 	err := NewEvent(corev1.EventTypeWarning, "otherReason", "this is an example error, %s", "yep")
 	if EventIs(err, NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "")) {
-		t.Error("did not expect event to be [Warn, ExampleStatusFailed]")
+		t.Error("Did not expect event to be [Warn, ExampleStatusFailed]")
 	}
 }
 
@@ -75,15 +75,15 @@ func TestNew_As(t *testing.T) {
 	err := NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "this is an example error, %s", "yep")
 
 	var event *ReconcilerEvent
-	if EventAs(err, &event) {
-		if event.EventType != "Warning" {
-			t.Error("mismatched reason")
-		}
-		if event.Reason != exampleStatusFailed {
-			t.Error("mismatched eventtype")
-		}
-	} else {
-		t.Error("expected error to be a ReconcilerEvent, is not")
+	if !EventAs(err, &event) {
+		t.Errorf("Expected error to be a ReconcilerEvent, is not")
+	}
+
+	if event.EventType != "Warning" {
+		t.Errorf("Mismatched EventType, expected Warning, got %s", event.EventType)
+	}
+	if event.Reason != exampleStatusFailed {
+		t.Errorf("Mismatched Reason, expected ExampleStatusFailed, got %s", event.Reason)
 	}
 }
 
@@ -92,16 +92,16 @@ func TestNil_As(t *testing.T) {
 
 	var event *ReconcilerEvent
 	if EventAs(err, &event) {
-		t.Error("did not expect error to be a ReconcilerEvent")
+		t.Error("Did not expect error to be a ReconcilerEvent")
 	}
 }
 
 func TestNew_Error(t *testing.T) {
 	err := NewEvent(corev1.EventTypeWarning, exampleStatusFailed, "this is an example error, %s", "yep")
 
-	want := "this is an example error, yep"
+	const want = "this is an example error, yep"
 	got := err.Error()
 	if diff := cmp.Diff(want, got); diff != "" {
-		t.Errorf("unexpected diff (-want, +got) = %v", diff)
+		t.Errorf("Unexpected diff (-want, +got) = %v", diff)
 	}
 }
