8f1363b18eb1c40a00b700206d6046835f0ac8d9
Scott Nichols <snichols@vmware.com>
2020-02-24 08:30:42 -0800

    new better way to test codegen

--- test/test-reconciler-codegen.sh
+++ test/test-reconciler-codegen.sh
@@ -25,9 +25,10 @@ KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 $(dir
 
 GENCLIENT_PKG=knative.dev/pkg/test/genclient
 
-(
-    $(dirname $0)/cleanup-test-codegen.sh
-)
+echo "Pre-deleting $(dirname $0)/genclient"
+rm -rf $(dirname $0)/genclient
+
+header "Test Generated Reconciler Builds."
 
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   ${GENCLIENT_PKG} knative.dev/pkg/apis/test \
@@ -52,3 +53,9 @@ ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
 
 
+if ! go build -v $(dirname $0)/genclient/... ; then
+    exit 1
+fi
+
+echo "Finished, deleting $(dirname $0)/genclient"
+rm -rf $(dirname $0)/genclient
