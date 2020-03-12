5a4145eca48b8a3f8e7e2c58e11f9213077c0fe5
Matt Moore <mattmoor@vmware.com>
2020-03-12 15:59:16 -0700

    Scope the webhook secret informers to the system namespace.

--- webhook/certificates/certificates_test.go
+++ webhook/certificates/certificates_test.go
@@ -23,7 +23,7 @@ import (
 	"time"
 
 	kubeclient "knative.dev/pkg/client/injection/kube/client/fake"
-	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/secret/fake"
+	_ "knative.dev/pkg/injection/clients/namespacedkube/informers/core/v1/secret/fake"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
