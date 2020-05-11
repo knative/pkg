02242f672adf6e37d05b477ccde9b0e181cbf62b
Yanwei Guo <yanweiguo@google.com>
2020-05-11 14:31:23 -0700

    dont record

--- metrics/config.go
+++ metrics/config.go
@@ -153,7 +153,12 @@ func NewStackdriverClientConfigFromMap(config map[string]string) *StackdriverCli
 // record applies the `ros` Options to each measurement in `mss` and then records the resulting
 // measurements in the metricsConfig's designated backend.
 func (mc *metricsConfig) record(ctx context.Context, mss []stats.Measurement, ros ...stats.Options) error {
-	if mc == nil || mc.recorder == nil {
+	if mc == nil {
+		// Do nothing if the metric config is not initialized yet. At that point, it's
+		// unclear whether should record or not.
+		return nil
+	}
+	if mc.recorder == nil {
 		return stats.RecordWithOptions(ctx, append(ros, stats.WithMeasurements(mss...))...)
 	}
 	return mc.recorder(ctx, mss, ros...)
