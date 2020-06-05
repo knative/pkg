86ef560b5e562222f64db4888ed5600be237a09d
Jennifer Power <barnabei.jennifer@gmail.com>
2020-06-05 16:13:46 -0400

    Applied changes to improve formatting and style

--- webhook/certificates/resources/certs_test.go
+++ webhook/certificates/resources/certs_test.go
@@ -62,7 +62,7 @@ func TestCreateCerts(t *testing.T) {
 	// Verify common name
 	const expectedCommonName = "got-the-hook.knative-webhook.svc"
 
-	if diff := cmp.Diff(caParsedCert.CommonName, expectedCommonName); diff != "" {
+	if caParsedCert.CommonName != expectedCommonName {
                 t.Fatalf("Unexpected Cert Common Name (-want +got) : %v", diff)
         }
 
