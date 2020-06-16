ffb929374a39cc0659b18100e9e2361f5106a81f
Antoine Cotten <hello@acotten.com>
2020-06-17 01:26:24 +0200

    Make Prometheus exporter port configurable via env (#1411)

--- metrics/config_test.go
+++ metrics/config_test.go
@@ -17,8 +17,11 @@ package metrics
 
 import (
 	"context"
+	"math"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -135,7 +138,7 @@ var (
 			Component:      testComponent,
 			PrometheusPort: 1023,
 		},
-		expectedErr: "invalid port 1023, should between 1024 and 65535",
+		expectedErr: "invalid port 1023, should be between 1024 and 65535",
 	}, {
 		name: "tooBigPrometheusPort",
 		ops: ExporterOptions{
@@ -146,7 +149,7 @@ var (
 			Component:      testComponent,
 			PrometheusPort: 65536,
 		},
-		expectedErr: "invalid port 65536, should between 1024 and 65535",
+		expectedErr: "invalid port 65536, should be between 1024 and 65535",
 	}}
 
 	successTests = []struct {
@@ -505,42 +508,6 @@ var (
 		},
 		expectedNewExporter: true,
 	}}
-
-	envTests = []struct {
-		name           string
-		ops            ExporterOptions
-		expectedConfig metricsConfig
-	}{{
-		name: "stackdriverFromEnv",
-		ops: ExporterOptions{
-			ConfigMap: map[string]string{},
-			Domain:    servingDomain,
-			Component: testComponent,
-		},
-		expectedConfig: metricsConfig{
-			domain:                            servingDomain,
-			component:                         testComponent,
-			backendDestination:                Stackdriver,
-			reportingPeriod:                   60 * time.Second,
-			isStackdriverBackend:              true,
-			stackdriverMetricTypePrefix:       path.Join(servingDomain, testComponent),
-			stackdriverCustomMetricTypePrefix: path.Join(customMetricTypePrefix, defaultCustomMetricSubDomain, testComponent),
-		},
-	}, {
-		name: "validPrometheus",
-		ops: ExporterOptions{
-			ConfigMap: map[string]string{BackendDestinationKey: string(Prometheus)},
-			Domain:    servingDomain,
-			Component: testComponent,
-		},
-		expectedConfig: metricsConfig{
-			domain:             servingDomain,
-			component:          testComponent,
-			backendDestination: Prometheus,
-			reportingPeriod:    5 * time.Second,
-			prometheusPort:     defaultPrometheusPort,
-		},
-	}}
 )
 
 func successTestsInit() {
@@ -574,9 +541,87 @@ func TestGetMetricsConfig(t *testing.T) {
 }
 
 func TestGetMetricsConfig_fromEnv(t *testing.T) {
-	os.Setenv(defaultBackendEnvName, "stackdriver")
-	for _, test := range envTests {
+	successTests := []struct {
+		name           string
+		varName        string
+		varValue       string
+		ops            ExporterOptions
+		expectedConfig metricsConfig
+	}{{
+		name:     "Stackdriver backend from env, no config",
+		varName:  defaultBackendEnvName,
+		varValue: string(Stackdriver),
+		ops: ExporterOptions{
+			ConfigMap: map[string]string{},
+			Domain:    servingDomain,
+			Component: testComponent,
+		},
+		expectedConfig: metricsConfig{
+			domain:                            servingDomain,
+			component:                         testComponent,
+			backendDestination:                Stackdriver,
+			reportingPeriod:                   60 * time.Second,
+			isStackdriverBackend:              true,
+			stackdriverMetricTypePrefix:       path.Join(servingDomain, testComponent),
+			stackdriverCustomMetricTypePrefix: path.Join(customMetricTypePrefix, defaultCustomMetricSubDomain, testComponent),
+		},
+	}, {
+		name:     "Stackdriver backend from env, Prometheus backend from config",
+		varName:  defaultBackendEnvName,
+		varValue: string(Stackdriver),
+		ops: ExporterOptions{
+			ConfigMap: map[string]string{BackendDestinationKey: string(Prometheus)},
+			Domain:    servingDomain,
+			Component: testComponent,
+		},
+		expectedConfig: metricsConfig{
+			domain:             servingDomain,
+			component:          testComponent,
+			backendDestination: Prometheus,
+			reportingPeriod:    5 * time.Second,
+			prometheusPort:     defaultPrometheusPort,
+		},
+	}, {
+		name:     "PrometheusPort from env",
+		varName:  prometheusPortEnvName,
+		varValue: "9999",
+		ops: ExporterOptions{
+			ConfigMap: map[string]string{},
+			Domain:    servingDomain,
+			Component: testComponent,
+		},
+		expectedConfig: metricsConfig{
+			domain:             servingDomain,
+			component:          testComponent,
+			backendDestination: Prometheus,
+			reportingPeriod:    5 * time.Second,
+			prometheusPort:     9999,
+		},
+	}}
+
+	failureTests := []struct {
+		name                string
+		varName             string
+		varValue            string
+		ops                 ExporterOptions
+		expectedErrContains string
+	}{{
+		name:     "Invalid PrometheusPort from env",
+		varName:  prometheusPortEnvName,
+		varValue: strconv.Itoa(math.MaxUint16 + 1),
+		ops: ExporterOptions{
+			ConfigMap: map[string]string{},
+			Domain:    servingDomain,
+			Component: testComponent,
+		},
+		expectedErrContains: "value out of range",
+	}}
+
+	for _, test := range successTests {
 		t.Run(test.name, func(t *testing.T) {
+			os.Setenv(test.varName, test.varValue)
+			defer os.Unsetenv(test.varName)
+
 			defer ClearAll()
 			mc, err := createMetricsConfig(test.ops, TestLogger(t))
 			if err != nil {
@@ -587,7 +632,22 @@ func TestGetMetricsConfig_fromEnv(t *testing.T) {
 			}
 		})
 	}
-	os.Unsetenv(defaultBackendEnvName)
+
+	for _, test := range failureTests {
+		t.Run(test.name, func(t *testing.T) {
+			os.Setenv(test.varName, test.varValue)
+			defer os.Unsetenv(test.varName)
+
+			defer ClearAll()
+			mc, err := createMetricsConfig(test.ops, TestLogger(t))
+			if mc != nil {
+				t.Errorf("Wanted no config, got %v", mc)
+			}
+			if err == nil || !strings.Contains(err.Error(), test.expectedErrContains) {
+				t.Errorf("Wanted err to contain: %q, got: %v", test.expectedErrContains, err)
+			}
+		})
+	}
 }
 
 func TestIsNewExporterRequiredFromNilConfig(t *testing.T) {
