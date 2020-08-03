7be5c0a87b000d0d539d1ea9ca4d4dd4578e902c
Jon Donovan <jondonovan@google.com>
2020-08-03 14:31:28 -0700

    Allow creating controller with custom RateLimiter. (#1546)
    * Allow creating controller with custom RateLimiter.
    
    Which was possible before via field modification.
    Not switching to a builder pattern mostly for speed of resolution.
    Happy to consider alternatives.
    
    * Add tests for new functionality.
    
    Specifically, these test that the Wait() function is notified about
    the item, and that the RateLimiter is passed through to the queue.
    
    * Add Options. Gophers love Options.
    
    * Even moar controller GenericOptions.
    
    * Attempt to appease lint, don't create struct for typecheck.
    
    * GenericOptions -> ControllerOptions
    
    * Public struct fields.

--- controller/two_lane_queue_test.go
+++ controller/two_lane_queue_test.go
@@ -22,10 +22,75 @@ import (
 	"time"
 
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/util/workqueue"
 )
 
+type chanRateLimiter struct {
+	t *testing.T
+	// Called when this ratelimiter is consulted for when to process a value.
+	whenCalled chan interface{}
+	retryCount map[interface{}]int
+}
+
+func (r chanRateLimiter) When(item interface{}) time.Duration {
+	r.whenCalled <- item
+	return 0 * time.Second
+}
+func (r chanRateLimiter) Forget(item interface{}) {
+	r.t.Fatalf("Forgetting item %+v, we should not be forgetting any items.", item)
+}
+func (r chanRateLimiter) NumRequeues(item interface{}) int {
+	return r.retryCount[item]
+}
+
+var _ workqueue.RateLimiter = &chanRateLimiter{}
+
+func TestRateLimit(t *testing.T) {
+	whenCalled := make(chan interface{}, 2)
+	rl := chanRateLimiter{
+		t,
+		whenCalled,
+		make(map[interface{}]int),
+	}
+	q := newTwoLaneWorkQueue("live-in-the-limited-lane", rl)
+	q.SlowLane().Add("1")
+	q.Add("2")
+
+	k, done := q.Get()
+	q.Done(k)
+	q.AddRateLimited(k)
+	rlK := <-whenCalled
+	if got, want := k.(string), rlK.(string); got != want {
+		t.Errorf(`Got = %q, want: "%q"`, got, want)
+	}
+	if done {
+		t.Error("The queue is unexpectedly shutdown")
+	}
+
+	k2, done := q.Get()
+	q.Done(k2)
+	q.AddRateLimited(k2)
+	rlK2 := <-whenCalled
+	if got, want := k2.(string), rlK2.(string); got != want {
+		t.Errorf(`Got = %q, want: "%q"`, got, want)
+	}
+	if s1, s2 := k.(string), k2.(string); (s1 != "1" || s2 != "2") && (s1 != "2" || s2 != "1") {
+		t.Errorf("Expected to see 1 and 2, instead saw %q and %q", s1, s2)
+	}
+	if done {
+		t.Error("The queue is unexpectedly shutdown")
+	}
+
+	// Queue has async moving parts so if we check at the wrong moment, this might still be 0 or 1.
+	if wait.PollImmediate(10*time.Millisecond, 250*time.Millisecond, func() (bool, error) {
+		return q.Len() == 2, nil
+	}) != nil {
+		t.Error("Queue length was never 2")
+	}
+}
+
 func TestSlowQueue(t *testing.T) {
-	q := newTwoLaneWorkQueue("live-in-the-fast-lane")
+	q := newTwoLaneWorkQueue("live-in-the-fast-lane", workqueue.DefaultControllerRateLimiter())
 	q.SlowLane().Add("1")
 	// Queue has async moving parts so if we check at the wrong moment, this might still be 0.
 	if wait.PollImmediate(10*time.Millisecond, 250*time.Millisecond, func() (bool, error) {
@@ -53,7 +118,7 @@ func TestSlowQueue(t *testing.T) {
 
 func TestDoubleKey(t *testing.T) {
 	// Verifies that we don't get double concurrent processing of the same key.
-	q := newTwoLaneWorkQueue("live-in-the-fast-lane")
+	q := newTwoLaneWorkQueue("live-in-the-fast-lane", workqueue.DefaultControllerRateLimiter())
 	q.Add("1")
 	t.Cleanup(q.ShutDown)
 
@@ -97,7 +162,7 @@ func TestDoubleKey(t *testing.T) {
 
 func TestOrder(t *testing.T) {
 	// Verifies that we read from the fast queue first.
-	q := newTwoLaneWorkQueue("live-in-the-fast-lane")
+	q := newTwoLaneWorkQueue("live-in-the-fast-lane", workqueue.DefaultControllerRateLimiter())
 	stop := make(chan struct{})
 	t.Cleanup(func() {
 		close(stop)
