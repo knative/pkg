de6106beec491adc05ce67108dffb50b498272fe
Josh Soref <jsoref@users.noreply.github.com>
2020-10-11 05:09:47 -0400

    spelling: mimic
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/gcs/mock/mock.go
+++ test/gcs/mock/mock.go
@@ -27,8 +27,8 @@ import (
 )
 
 // I don't know if it is easier or not to use go mock, but we really only need two things:
-// 1) Ability to mimick creation of buckets and objects
-// 2) Ability to mimick returning errors
+// 1) Ability to mimic creation of buckets and objects
+// 2) Ability to mimic returning errors
 //
 // We don't need arbitrary return values, so generators like go mock or testify might be
 // overkill and doesn't give us the flexibility we need (e.g., will have to specify and
