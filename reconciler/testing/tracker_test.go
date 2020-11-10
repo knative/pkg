cc77cba3ea12b4c77cdcb2469b1a515f6d7db668
Markus Thömmes <markusthoemmes@me.com>
2020-11-10 19:19:59 +0100

    Add GetObservers to tracker interface (#1896)
    * Add GetObservers to tracker interface
    
    This allows an arbitrary client to fetch all the observers of a given object. Ultimately, this enables an informer setup that can use this knowledge as a filter to decide whether or not the current object is actually watched by anything.
    
    * Fix log line

--- reconciler/testing/tracker_test.go
+++ reconciler/testing/tracker_test.go
@@ -19,6 +19,7 @@ import (
 	"testing"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	. "knative.dev/pkg/testing"
 	"knative.dev/pkg/tracker"
 )
@@ -30,24 +31,52 @@ func TestFakeTracker(t *testing.T) {
 			Name:      "foo",
 		},
 	}
+	t1Name := types.NamespacedName{
+		Namespace: t1.Namespace,
+		Name:      t1.Name,
+	}
 	t2 := &Resource{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: "default",
 			Name:      "bar.baz.this-is-fine",
 		},
 	}
+	t2Name := types.NamespacedName{
+		Namespace: t2.Namespace,
+		Name:      t2.Name,
+	}
 
+	obj1 := &Resource{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "fakeapi",
+			Kind:       "Fake",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "foo",
+			Name:      "bar",
+		},
+	}
 	ref1 := tracker.Reference{
-		APIVersion: "fakeapi",
-		Kind:       "Fake",
-		Namespace:  "foo",
-		Name:       "bar",
+		APIVersion: obj1.APIVersion,
+		Kind:       obj1.Kind,
+		Namespace:  obj1.Namespace,
+		Name:       obj1.Name,
+	}
+	obj2 := &Resource{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "fakeapi2",
+			Kind:       "Fake2",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "foo2",
+			Name:      "bar2",
+		},
 	}
 	ref2 := tracker.Reference{
-		APIVersion: "fakeapi2",
-		Kind:       "Fake2",
-		Namespace:  "foo2",
-		Name:       "bar2",
+		APIVersion: obj2.APIVersion,
+		Kind:       obj2.Kind,
+		Namespace:  obj2.Namespace,
+		Name:       obj2.Name,
 	}
 
 	trk := &FakeTracker{}
@@ -57,9 +86,16 @@ func TestFakeTracker(t *testing.T) {
 	if !isTracking(trk, ref1) {
 		t.Fatal("Tracker is not tracking", ref1)
 	}
+	if !hasObserver(trk, obj1, t1Name) {
+		t.Fatalf("Object %v is not being observed by %v", obj1, t1Name)
+	}
+
 	trk.OnDeletedObserver(t1)
 	if isTracking(trk, ref1) {
 		t.Fatal("Tracker is still tracking", ref1)
+	}
+	if hasObserver(trk, obj1, t1Name) {
+		t.Fatalf("Object %v is still being observed by %v", obj1, t1Name)
 	}
 
 	// Adding t1, t2 to ref1 and t2 to ref2, then removing t2 results in ref2 stopping
@@ -73,22 +109,58 @@ func TestFakeTracker(t *testing.T) {
 	if !isTracking(trk, ref2) {
 		t.Fatal("Tracker is not tracking", ref2)
 	}
+	if !hasObserver(trk, obj1, t1Name) {
+		t.Fatalf("Object %v is not being observed by %v", obj1, t1Name)
+	}
+	if !hasObserver(trk, obj1, t2Name) {
+		t.Fatalf("Object %v is not being observed by %v", obj1, t2Name)
+	}
+	if hasObserver(trk, obj2, t1Name) {
+		t.Fatalf("Object %v wrongly being observed by %v", obj2, t1Name)
+	}
+	if !hasObserver(trk, obj2, t2Name) {
+		t.Fatalf("Object %v is not being observed by %v", obj2, t2Name)
+	}
+
 	trk.OnDeletedObserver(t2)
 	if !isTracking(trk, ref1) {
 		t.Fatal("Tracker is not tracking", ref1)
 	}
+	if !hasObserver(trk, obj1, t1Name) {
+		t.Fatalf("Object %v is not being observed by %v", obj1, t1Name)
+	}
 	if isTracking(trk, ref2) {
 		t.Fatal("Tracker is still tracking", ref2)
 	}
+	if hasObserver(trk, obj1, t2Name) {
+		t.Fatalf("Object %v is still being observed by %v", obj1, t2Name)
+	}
+	if hasObserver(trk, obj2, t2Name) {
+		t.Fatalf("Object %v is still being observed by %v", obj2, t2Name)
+	}
+
 	trk.OnDeletedObserver(t1)
 	if isTracking(trk, ref1) {
 		t.Fatal("Tracker is still tracking", ref1)
+	}
+	if hasObserver(trk, obj1, t1Name) {
+		t.Fatalf("Object %v is still being observed by %v", obj1, t1Name)
 	}
 }
 
 func isTracking(tracker *FakeTracker, ref1 tracker.Reference) bool {
 	for _, tracking := range tracker.References() {
 		if tracking == ref1 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasObserver(tracker *FakeTracker, obj *Resource, obs types.NamespacedName) bool {
+	observers := tracker.GetObservers(obj)
+	for _, observer := range observers {
+		if observer == obs {
 			return true
 		}
 	}
