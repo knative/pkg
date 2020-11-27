0d896b5c87b8d9225999b97055201b2405ba5595
Glyn Normington <glyn.normington.work@gmail.com>
2020-11-27 01:33:35 +0000

    Apply multiple bindings to the same subject (#1925)
    * Factor index out of Reconciler
    
    Applying multiple Bindings to the same subject makes the reconciler more
    complex, so split out the index handling.
    
    * unexport contents of index.go
    
    * Apply multiple bindings to the same subject
    
    The affected files are actually copyright 2019-2020,
    but boilerplate-check fails if this is used. See:
    https://github.com/mattmoor/boilerplate-check/issues/22
    
    Fixes https://github.com/knative/pkg/issues/1786

--- webhook/psbinding/index_test.go
+++ webhook/psbinding/index_test.go
@@ -34,9 +34,6 @@ func TestExact(t *testing.T) {
 			Namespace: "blah",
 			Name:      "asdf",
 		},
-		Spec: TestBindableSpec{
-			Foo: "bar",
-		},
 	}
 
 	gvk := want.GetGroupVersionKind()
@@ -47,19 +44,18 @@ func TestExact(t *testing.T) {
 		Name:      want.GetName(),
 	}
 
-	// Before we Add something, we shouldn't be able to Get anything.
-	if got, ok := em.Get(key); ok {
-		t.Errorf("Get(%+v) = %v, %v; wanted nil, false", key, got, ok)
+	// Before we add something, we shouldn't be able to get anything.
+	if got := em.get(key); len(got) != 0 {
+		t.Errorf("Get(%+v) = %v; wanted empty list", key, got)
 	}
 
-	// Now Add it.
-	em.Add(key, want)
+	// Now add it.
+	em.add(key, want)
 
-	// After we Add something, we should be able to Get it.
-	if got, ok := em.Get(key); !ok {
-		t.Errorf("Get(%+v) = %v, %v; wanted b, true", key, want, ok)
-	} else if !cmp.Equal(got, want) {
-		t.Error("Get (-want, +got):", cmp.Diff(want, got))
+	// After we add something, we should be able to get it.
+	wanted := []Bindable{want}
+	if got := em.get(key); !cmp.Equal(got, wanted) {
+		t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
 	}
 
 	otherKey := exactKey{
@@ -69,9 +65,24 @@ func TestExact(t *testing.T) {
 		Name:      "bar",
 	}
 
-	// After we Add something, we still shouldn't return things for other keys.
-	if got, ok := em.Get(otherKey); ok {
-		t.Errorf("Get(%+v) = %v, %v; wanted nil, false", key, got, ok)
+	// After we add something, we still shouldn't return things for other keys.
+	if got := em.get(otherKey); len(got) != 0 {
+		t.Errorf("Get(%+v) = %v; wanted empty list", key, got)
+	}
+
+	// Add another Binding with the same coordinates.
+	alsoWant := &TestBindable{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "blah",
+			Name:      "asdf",
+		},
+	}
+	em.add(key, want)
+
+	// We should now get both Bindings.
+	wanted = []Bindable{want, alsoWant}
+	if got := em.get(key); !cmp.Equal(got, wanted) {
+		t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
 	}
 }
 
@@ -85,9 +96,6 @@ func TestInexact(t *testing.T) {
 				"baz": "blah",
 			},
 		},
-		Spec: TestBindableSpec{
-			Foo: "bar",
-		},
 	}
 	ls := labels.Set(want.Labels)
 
@@ -100,8 +108,8 @@ func TestInexact(t *testing.T) {
 
 	t.Run("empty matcher doesn't match", func(t *testing.T) {
 		im := make(inexactMatcher, 1)
-		if got, ok := im.Get(key, ls); ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted nil, false", key, got, ok)
+		if got := im.get(key, ls); len(got) != 0 {
+			t.Errorf("Get(%+v) = %v; wanted empty list", key, got)
 		}
 	})
 
@@ -111,13 +119,12 @@ func TestInexact(t *testing.T) {
 		// Use exactly the labels from the resource.
 		selector := ls.AsSelector()
 
-		im.Add(key, selector, want)
+		im.add(key, selector, want)
 
 		// With an appropriate selector, we match and get the binding.
-		if got, ok := im.Get(key, ls); !ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted b, true", key, want, ok)
-		} else if !cmp.Equal(got, want) {
-			t.Error("Get (-want, +got):", cmp.Diff(want, got))
+		wanted := []Bindable{want}
+		if got := im.get(key, ls); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
 		}
 	})
 
@@ -127,13 +134,12 @@ func TestInexact(t *testing.T) {
 		// Match everything.
 		selector := labels.Everything()
 
-		im.Add(key, selector, want)
+		im.add(key, selector, want)
 
 		// With an appropriate selector, we match and get the binding.
-		if got, ok := im.Get(key, ls); !ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted b, true", key, want, ok)
-		} else if !cmp.Equal(got, want) {
-			t.Error("Get (-want, +got):", cmp.Diff(want, got))
+		wanted := []Bindable{want}
+		if got := im.get(key, ls); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
 		}
 	})
 
@@ -143,10 +149,10 @@ func TestInexact(t *testing.T) {
 		// Match nothing.
 		selector := labels.Nothing()
 
-		im.Add(key, selector, want)
+		im.add(key, selector, want)
 
-		if got, ok := im.Get(key, ls); ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted nil, false", key, got, ok)
+		if got := im.get(key, ls); len(got) != 0 {
+			t.Errorf("Get(%+v) = %v; wanted empty list", key, got)
 		}
 	})
 
@@ -158,13 +164,12 @@ func TestInexact(t *testing.T) {
 			"foo": "bar",
 		}).AsSelector()
 
-		im.Add(key, selector, want)
+		im.add(key, selector, want)
 
 		// With an appropriate selector, we match and get the binding.
-		if got, ok := im.Get(key, ls); !ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted b, true", key, want, ok)
-		} else if !cmp.Equal(got, want) {
-			t.Error("Get (-want, +got):", cmp.Diff(want, got))
+		wanted := []Bindable{want}
+		if got := im.get(key, ls); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
 		}
 	})
 
@@ -177,11 +182,11 @@ func TestInexact(t *testing.T) {
 			"not": "found",
 		}).AsSelector()
 
-		im.Add(key, selector, want)
+		im.add(key, selector, want)
 
 		// We shouldn't match because the second labels shouldn't match.
-		if got, ok := im.Get(key, ls); ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted nil, false", key, got, ok)
+		if got := im.get(key, ls); len(got) != 0 {
+			t.Errorf("Get(%+v) = %v; wanted empty list", key, got)
 		}
 	})
 
@@ -191,14 +196,184 @@ func TestInexact(t *testing.T) {
 		// Use exactly the labels from the resource.
 		selector := ls.AsSelector()
 
-		im.Add(key, selector, want)
+		im.add(key, selector, want)
 
 		otherKey := key
 		otherKey.Namespace = "another"
 
 		// We shouldn't match because the second labels shouldn't match.
-		if got, ok := im.Get(otherKey, ls); ok {
-			t.Errorf("Get(%+v) = %v, %v; wanted nil, false", key, got, ok)
+		if got := im.get(otherKey, ls); len(got) != 0 {
+			t.Errorf("Get(%+v) = %v; wanted empty list", key, got)
+		}
+	})
+
+	t.Run("multiple Bindings match", func(t *testing.T) {
+		alsoWant := &TestBindable{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "blah",
+				Name:      "asdf",
+				Labels: map[string]string{
+					"foo": "bar",
+					"baz": "blah",
+				},
+			},
+		}
+
+		im := make(inexactMatcher, 1)
+
+		// Use a subset of the resources labels.
+		selector := labels.Set(map[string]string{
+			"foo": "bar",
+		}).AsSelector()
+
+		im.add(key, selector, want)
+		im.add(key, selector, alsoWant)
+
+		// With an appropriate selector, we match and get both bindings.
+		wanted := []Bindable{want, alsoWant}
+		if got := im.get(key, ls); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
+		}
+	})
+}
+
+func TestIndex(t *testing.T) {
+	wantExact1 := &TestBindable{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "a",
+			Name:      "b",
+			Labels: map[string]string{
+				"colour": "red",
+			},
+		},
+	}
+
+	wantExact2 := &TestBindable{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "a",
+			Name:      "b",
+			Labels: map[string]string{
+				"colour": "blue",
+			},
+		},
+	}
+
+	wantInexact1 := &TestBindable{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "a",
+			Name:      "b",
+			Labels: map[string]string{
+				"colour": "red",
+			},
+		},
+	}
+
+	wantInexact2 := &TestBindable{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "a",
+			Name:      "b",
+			Labels: map[string]string{
+				"colour": "blue",
+			},
+		},
+	}
+
+	gvk := wantExact1.GetGroupVersionKind()
+	eKey := exactKey{
+		Group:     gvk.Group,
+		Kind:      gvk.Kind,
+		Namespace: wantExact1.GetNamespace(),
+		Name:      wantExact1.GetName(),
+	}
+	iKey := inexactKey{
+		Group:     gvk.Group,
+		Kind:      gvk.Kind,
+		Namespace: wantExact1.GetNamespace(),
+	}
+	redLabels := labels.Set(wantInexact1.Labels)
+	redSelector := redLabels.AsSelector()
+	blueLabels := labels.Set(wantInexact2.Labels)
+	blueSelector := blueLabels.AsSelector()
+	none := make(labels.Set)
+	allSelector := none.AsSelector()
+
+	t.Run("single exact match is found", func(t *testing.T) {
+		var index index
+		newIndexBuilder().
+			associate(eKey, wantExact1).
+			build(&index)
+
+		wanted := []Bindable{wantExact1}
+		if got := index.lookUp(eKey, none); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
+		}
+	})
+
+	t.Run("single inexact match is found", func(t *testing.T) {
+		var index index
+		newIndexBuilder().
+			associateSelection(iKey, redSelector, wantInexact1).
+			build(&index)
+
+		wanted := []Bindable{wantInexact1}
+		if got := index.lookUp(eKey, redLabels); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
+		}
+	})
+
+	t.Run("multiple exact matches are found", func(t *testing.T) {
+		var index index
+		newIndexBuilder().
+			associate(eKey, wantExact1).
+			associate(eKey, wantExact2).
+			build(&index)
+
+		wanted := []Bindable{wantExact1, wantExact2}
+		if got := index.lookUp(eKey, none); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
+		}
+	})
+
+	t.Run("multiple inexact matches are found", func(t *testing.T) {
+		var index index
+		newIndexBuilder().
+			associateSelection(iKey, allSelector, wantInexact1).
+			associateSelection(iKey, allSelector, wantInexact2).
+			build(&index)
+
+		wanted := []Bindable{wantInexact1, wantInexact2}
+		if got := index.lookUp(eKey, redLabels); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
+		}
+	})
+
+	t.Run("multiple exact and inexact matches are found", func(t *testing.T) {
+		var index index
+		newIndexBuilder().
+			associate(eKey, wantExact1).
+			associate(eKey, wantExact2).
+			associateSelection(iKey, allSelector, wantInexact1).
+			associateSelection(iKey, allSelector, wantInexact2).
+			build(&index)
+
+		wanted := []Bindable{wantExact1, wantExact2, wantInexact1, wantInexact2}
+		if got := index.lookUp(eKey, none); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
+		}
+	})
+
+	t.Run("the correct exact and inexact matches are found", func(t *testing.T) {
+		var index index
+		newIndexBuilder().
+			associate(eKey, wantExact1).
+			associate(eKey, wantExact2).
+			associateSelection(iKey, redSelector, wantInexact1).
+			associateSelection(iKey, blueSelector, wantInexact2).
+			build(&index)
+
+		wanted := []Bindable{wantExact1, wantExact2, wantInexact1}
+		if got := index.lookUp(eKey, redLabels); !cmp.Equal(got, wanted) {
+			t.Error("Get (-want, +got):", cmp.Diff(wanted, got))
 		}
 	})
 }
