ce071b4e906b9a6f6a514b9bfa7814f1acd8c7ac
kthakar <kthakar@us.ibm.com>
2021-02-09 09:47:32 -0500

    updates in format

--- source/source_stats_reporter.go
+++ source/source_stats_reporter.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-				http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,7 +34,7 @@ var (
 		"Number of events sent",
 		stats.UnitDimensionless,
 	)
-	
+
 	// RetryEventCountM is a counter which records the number of events sent by the source in retries.
 	RetryEventCountM = stats.Int64(
 		"retry_event_count",
