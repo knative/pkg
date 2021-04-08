345beeb16fc0b46bbd369fa0293f182ca30a1a4f
Markus Thömmes <markusthoemmes@me.com>
2021-04-08 13:50:46 +0200

    Add possibility to implement a deletion handler in a reconciler.
    ObserveFinalizeKind is not guaranteed to be called for every resource
    that gets deleted, even if they do have a finalizer. The leader can race
    the observers in removing the finalizer so the observers would not even
    see the deletion.
    
    Using the OnDelete handler on the informer is equally racy in that it
    can trigger the deletion handler while a potential "normal" reconcile is
    still in-flight.
    
    This adds an ObserveDeletedKind handler that can be implemented by
    reconcilers. The events still go via the workQueue, so proper order and
    deduplication is guaranteed. Observation is guaranteed as well. In most
    if not all cases, this handler should replace the ObserveFinalizeKind
    handler.

--- reconciler/configstore.go
+++ reconciler/configstore.go
@@ -16,10 +16,26 @@ limitations under the License.
 
 package reconciler
 
-import "context"
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/types"
+)
 
 // ConfigStore is used to attach the frozen configuration to the context.
 type ConfigStore interface {
 	// ConfigStore is used to attach the frozen configuration to the context.
 	ToContext(ctx context.Context) context.Context
+}
+
+// OnDeletionInterface defines the strongly typed interface to be implemented by a
+// controller observing a deletion of {{.type|raw}}. Every controller that was active
+// during the deletion of the respective resource is guaranteed to observe this event,
+// leader or not. It's usually used to clear up in-memory state regarding the respective
+// resource. Finalizers should be used to ensure external resources are properly cleaned
+// up.
+type OnDeletionInterface interface {
+	// ObserveDeletion implements custom logic to observe deletion of the respective resource
+	// with the given key.
+	ObserveDeletion(ctx context.Context, key types.NamespacedName) error
 }
