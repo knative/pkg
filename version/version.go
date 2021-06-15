535e58b7e302858b6d08531dd48132007bae27cb
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-06-15 17:04:51 +0900

    Fix link

--- version/version.go
+++ version/version.go
@@ -32,7 +32,7 @@ const (
 
 	// NOTE: If you are changing this line, please also update the minimum kubernetes
 	// version listed here:
-	// https://github.com/knative/docs/blob/main/docs/install/any-kubernetes-cluster.md#before-you-begin
+	// https://github.com/knative/docs/blob/main/docs/install/prerequisites.md#prerequisites
 	// https://github.com/knative/docs/blob/main/docs/install/knative-with-operators.md#prerequisites
 	defaultMinimumVersion = "v1.19.0"
 )
