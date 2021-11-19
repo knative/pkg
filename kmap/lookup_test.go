d0cfd6e6449cb1e72b38f9e23a4a7a55103ff601
dprotaso <dprotaso@gmail.com>
2021-11-19 17:36:03 -0500

    Rename OrderedLookup to be KeyPriority
    - make it a type alias of a string array
    - add a update key function that can be used for migration

--- kmap/lookup_test.go
+++ kmap/lookup_test.go
@@ -22,49 +22,31 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestNewOrderedLookup(t *testing.T) {
-	keys := []string{"1", "2", "3"}
-	a := NewOrderedLookup(keys...)
-
-	if diff := cmp.Diff(keys, a.Keys); diff != "" {
-		t.Error("NewOrderedLookup unexpected diff (-want, +got):", diff)
-	}
-}
-
-func TestNewOrderedLookup_BadInput(t *testing.T) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Error("expected no keys to panic")
-		}
-	}()
-	NewOrderedLookup()
-}
-
-func TestOrderedLookup_Get(t *testing.T) {
+func TestKeyPriority(t *testing.T) {
 	tests := []struct {
 		name     string
-		keys     []string
+		keys     KeyPriority
 		in       map[string]string
 		outKey   string
 		outValue string
 		outOk    bool
 	}{{
 		name:     "single key in map",
-		keys:     []string{"old", "new"},
+		keys:     KeyPriority{"old", "new"},
 		in:       map[string]string{"old": "1"},
 		outKey:   "old",
 		outValue: "1",
 		outOk:    true,
 	}, {
 		name:     "another single key in map",
-		keys:     []string{"old", "new"},
+		keys:     KeyPriority{"old", "new"},
 		in:       map[string]string{"new": "1"},
 		outKey:   "new",
 		outValue: "1",
 		outOk:    true,
 	}, {
 		name:     "lower ordinal takes priority",
-		keys:     []string{"old", "new"},
+		keys:     KeyPriority{"old", "new"},
 		in:       map[string]string{"new": "1", "old": "2"},
 		outKey:   "old",
 		outValue: "2",
@@ -82,9 +64,7 @@ func TestOrderedLookup_Get(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			a := NewOrderedLookup(tc.keys...)
-
-			k, v, ok := a.Get(tc.in)
+			k, v, ok := tc.keys.Get(tc.in)
 
 			if tc.outOk != ok {
 				t.Error("expected ok to be", tc.outOk)
@@ -98,7 +78,54 @@ func TestOrderedLookup_Get(t *testing.T) {
 				t.Errorf("expected key to be %q got: %q", tc.outKey, k)
 			}
 
+			if want, got := v, tc.keys.Value(tc.in); got != want {
+				t.Errorf("Value() diff %q != %q", want, got)
+			}
 		})
 	}
+}
 
+func TestUpdateKeys(t *testing.T) {
+	tests := []struct {
+		name string
+		in   map[string]string
+		out  map[string]string
+		ols  []KeyPriority
+	}{{
+		name: "identity",
+		in:   map[string]string{"1": "2"},
+		out:  map[string]string{"1": "2"},
+	}, {
+		name: "single replacement",
+		in:   map[string]string{"1": "2", "4": "5"},
+		ols: []KeyPriority{
+			{"3", "1"},
+		},
+		out: map[string]string{"3": "2", "4": "5"},
+	}, {
+		name: "multiple replacement",
+		in:   map[string]string{"1": "2", "6": "5", "8": "9"},
+		ols: []KeyPriority{
+			{"3", "1"},
+			{"4", "6"},
+		},
+		out: map[string]string{"3": "2", "4": "5", "8": "9"},
+	}, {
+		name: "default not replaced",
+		in:   map[string]string{"1": "2", "4": "5"},
+		ols: []KeyPriority{
+			{"1", "3"},
+		},
+		out: map[string]string{"1": "2", "4": "5"},
+	}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+
+			out := UpdateKeys(tc.in, tc.ols...)
+			if diff := cmp.Diff(tc.out, out); diff != "" {
+				t.Error("Migrate diff (-want,+got):", diff)
+			}
+		})
+	}
 }
