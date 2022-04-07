29f716f18bbc83f7130d0bbbc27e4bb54bbdcd61
Nghia Tran <tcnghia@gmail.com>
2022-04-07 09:45:44 -0700

    Fix `InitialBuckets()` for statefulSetBuilder's electors (#2483)
    * Not all unoppossedElector has universal bucket
    
    Some are for StatefulSet and only owns their own
    
    * Avoid comparing pointers which may be flaky
    
    * Address PR feedback

--- leaderelection/context_test.go
+++ leaderelection/context_test.go
@@ -384,8 +384,10 @@ func TestStandardBucketName(t *testing.T) {
 }
 
 func TestUnopposedElectorInitialBucket(t *testing.T) {
-	u := &unopposedElector{}
-	if u.InitialBuckets()[0] != reconciler.UniversalBucket() {
-		t.Errorf("expected the unopposedElector to return the universal bucket")
+	u := &unopposedElector{
+		bkt: reconciler.UniversalBucket(),
+	}
+	if diff := cmp.Diff(u.InitialBuckets(), []reconciler.Bucket{reconciler.UniversalBucket()}); diff != "" {
+		t.Errorf("unexpected u.InitialBuckets() (-want,+got): %s", diff)
 	}
 }
