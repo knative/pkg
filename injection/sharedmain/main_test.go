af54d7330b784830a0c121fa29744d3c9fa45f10
Matt Moore <mattmoor@chainguard.dev>
2022-05-03 08:25:57 -0700

    Support fetching configs from `ctx`. (#2505)
    * Support fetching configs from `ctx`.
    
    This enables callers of `sharedmain` to infuse `ctx` with a `logging.Config` or `leaderelection.Config` instead of relying on the API server for it.
    
    * Move context methods into appropriate packages, drop stutter

--- injection/sharedmain/main_test.go
+++ injection/sharedmain/main_test.go
@@ -17,11 +17,15 @@ limitations under the License.
 package sharedmain
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"go.uber.org/zap/zapcore"
 
 	"knative.dev/pkg/injection"
+	"knative.dev/pkg/leaderelection"
+	"knative.dev/pkg/logging"
 )
 
 func TestEnabledControllers(t *testing.T) {
@@ -30,29 +34,26 @@ func TestEnabledControllers(t *testing.T) {
 		disabledControllers []string
 		ctors               []injection.NamedControllerConstructor
 		wantNames           []string
-	}{
-		{
-			name:                "zero",
-			disabledControllers: []string{"foo"},
-			ctors:               []injection.NamedControllerConstructor{{Name: "bar"}},
-			wantNames:           []string{"bar"},
+	}{{
+		name:                "zero",
+		disabledControllers: []string{"foo"},
+		ctors:               []injection.NamedControllerConstructor{{Name: "bar"}},
+		wantNames:           []string{"bar"},
+	}, {
+		name:                "one",
+		disabledControllers: []string{"foo"},
+		ctors:               []injection.NamedControllerConstructor{{Name: "foo"}},
+		wantNames:           []string{},
+	}, {
+		name:                "two",
+		disabledControllers: []string{"foo"},
+		ctors: []injection.NamedControllerConstructor{
+			{Name: "foo"},
+			{Name: "bar"},
 		},
-		{
-			name:                "one",
-			disabledControllers: []string{"foo"},
-			ctors:               []injection.NamedControllerConstructor{{Name: "foo"}},
-			wantNames:           []string{},
-		},
-		{
-			name:                "two",
-			disabledControllers: []string{"foo"},
-			ctors: []injection.NamedControllerConstructor{
-				{Name: "foo"},
-				{Name: "bar"},
-			},
-			wantNames: []string{"bar"},
-		},
-	}
+		wantNames: []string{"bar"},
+	}}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := enabledControllers(tt.disabledControllers, tt.ctors)
@@ -69,4 +70,36 @@ func namesOf(ctors []injection.NamedControllerConstructor) []string {
 		names = append(names, x.Name)
 	}
 	return names
+}
+
+func TestWithLoggingConfig(t *testing.T) {
+	want := &logging.Config{
+		LoggingLevel: map[string]zapcore.Level{
+			"foo": zapcore.DebugLevel,
+		},
+	}
+	ctx := logging.WithConfig(context.Background(), want)
+
+	got, err := GetLoggingConfig(ctx)
+	if err != nil {
+		t.Fatalf("GetLoggingConfig() = %v", err)
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("(-got, +want) = %s", diff)
+	}
+}
+
+func TestWithLeaderElectionConfig(t *testing.T) {
+	want := &leaderelection.Config{
+		Buckets: 12,
+	}
+	ctx := leaderelection.WithConfig(context.Background(), want)
+
+	got, err := GetLeaderElectionConfig(ctx)
+	if err != nil {
+		t.Fatalf("GetLeaderElectionConfig() = %v", err)
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("(-got, +want) = %s", diff)
+	}
 }
