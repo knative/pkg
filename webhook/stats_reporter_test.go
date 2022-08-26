93b66e6a8700ca4d50e42cd2d2ae423454adcb12
Kenny Leung <kleung@chainguard.dev>
2022-08-26 09:29:20 -0700

    Update: report stats for request (#2584)
    * report stats for request
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    * fix ref
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    * fix import
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    * update
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    * gofmt
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    * fix test
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    * fix boiler
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- webhook/stats_reporter_test.go
+++ webhook/stats_reporter_test.go
@@ -22,12 +22,13 @@ import (
 	"time"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	apixv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/pkg/metrics/metricstest"
 	_ "knative.dev/pkg/metrics/testing"
 )
 
-func TestWebhookStatsReporter(t *testing.T) {
+func TestWebhookStatsReporterAdmission(t *testing.T) {
 	setup()
 	req := &admissionv1.AdmissionRequest{
 		UID:       "705ab4f5-6393-11e8-b7cc-42010a800002",
@@ -58,8 +59,45 @@ func TestWebhookStatsReporter(t *testing.T) {
 		admissionAllowedKey.Name():  strconv.FormatBool(resp.Allowed),
 	}
 
-	r.ReportRequest(req, resp, time.Duration(shortTime)*time.Millisecond)
-	r.ReportRequest(req, resp, time.Duration(longTime)*time.Millisecond)
+	if err := r.ReportAdmissionRequest(req, resp, time.Duration(shortTime)*time.Millisecond); err != nil {
+		t.Fatalf("ReportAdmissionRequest() = %v", err)
+	}
+	if err := r.ReportAdmissionRequest(req, resp, time.Duration(longTime)*time.Millisecond); err != nil {
+		t.Fatalf("ReportAdmissionRequest() = %v", err)
+	}
+
+	metricstest.CheckCountData(t, requestCountName, expectedTags, 2)
+	metricstest.CheckDistributionData(t, requestLatenciesName, expectedTags, 2, shortTime, longTime)
+}
+
+func TestWebhookStatsReporterConversion(t *testing.T) {
+	setup()
+	req := &apixv1.ConversionRequest{
+		UID:               "705ab4f5-6393-11e8-b7cc-42010a800003",
+		DesiredAPIVersion: "knative.dev/v1",
+	}
+
+	resp := &apixv1.ConversionResponse{
+		UID:    req.UID,
+		Result: metav1.Status{Status: "Failure", Reason: metav1.StatusReasonNotFound, Code: 404},
+	}
+
+	r, _ := NewStatsReporter()
+
+	shortTime, longTime := 1100.0, 9100.0
+	expectedTags := map[string]string{
+		desiredAPIVersionKey.Name(): req.DesiredAPIVersion,
+		resultStatusKey.Name():      resp.Result.Status,
+		resultReasonKey.Name():      string(resp.Result.Reason),
+		resultCodeKey.Name():        strconv.Itoa(int(resp.Result.Code)),
+	}
+
+	if err := r.ReportConversionRequest(req, resp, time.Duration(shortTime)*time.Millisecond); err != nil {
+		t.Fatalf("ReportConversionRequest() = %v", err)
+	}
+	if err := r.ReportConversionRequest(req, resp, time.Duration(longTime)*time.Millisecond); err != nil {
+		t.Fatalf("ReportConversionRequest() = %v", err)
+	}
 
 	metricstest.CheckCountData(t, requestCountName, expectedTags, 2)
 	metricstest.CheckDistributionData(t, requestLatenciesName, expectedTags, 2, shortTime, longTime)
