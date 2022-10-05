578f53cd23837d2b19f6ea75cfa505dcf6709f9a
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-10-05 09:42:05 +0200

    Preserve webhook namespaceSelector.matchLabels
    I have a webhook with this definition and the reconciler is
    removing the matchLabels field:
    
    Current resource:
    ```
    				  namespaceSelector:
    				    matchExpressions:
    				    - key: webhooks.knative.dev/exclude
    				      operator: DoesNotExist
    				  objectSelector:
    				    matchLabels:
    				      app.kubernetes.io/component: kafka-dispatcher
    
    ```
    
    Applied resource:
    ```
    				    namespaceSelector:
    				      matchExpressions: [ ]
    				      matchLabels:
    				        app.kubernetes.io/name: knative-eventing
    				    objectSelector:
    				      matchLabels:
    				        app.kubernetes.io/component: kafka-dispatcher
    ```
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- webhook/helper.go
+++ webhook/helper.go
@@ -34,16 +34,13 @@ func EnsureLabelSelectorExpressions(
 		return want
 	}
 
-	if len(current.MatchExpressions) == 0 {
-		return want
-	}
-
 	var wantExpressions []metav1.LabelSelectorRequirement
 	if want != nil {
 		wantExpressions = want.MatchExpressions
 	}
 
 	return &metav1.LabelSelector{
+		MatchLabels: current.MatchLabels,
 		MatchExpressions: ensureLabelSelectorRequirements(
 			current.MatchExpressions, wantExpressions),
 	}
