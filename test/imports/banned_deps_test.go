89284337823b34236af487bd2309d1a715a6b182
dprotaso <dprotaso@gmail.com>
2022-11-22 14:17:30 -0500

    gofmt with go1.19

--- test/imports/banned_deps_test.go
+++ test/imports/banned_deps_test.go
@@ -44,9 +44,12 @@ var bannedImports = []vbTuple{
 }
 
 // TestBannedImports run with:
-//   go test ./vendor/knative.dev/pkg/test/imports/ -run TestBannedImports
+//
+//	go test ./vendor/knative.dev/pkg/test/imports/ -run TestBannedImports
+//
 // Or with test-infra:
-//   report_go_test ${REPO_ROOT_DIR}/vendor/knative.dev/pkg/test/imports/...
+//
+//	report_go_test ${REPO_ROOT_DIR}/vendor/knative.dev/pkg/test/imports/...
 func TestBannedImports(t *testing.T) {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(filename)
