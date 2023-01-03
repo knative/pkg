d6ea68f2f1a2c1e3e4a2ce874cde6d8c2a172b18
Martin Gencur <mgencur@redhat.com>
2023-01-03 10:18:41 +0100

    Reduce background_verif_test

--- test/upgrade/background_verification_test.go
+++ test/upgrade/background_verification_test.go
@@ -18,77 +18,12 @@ package upgrade_test
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"knative.dev/pkg/test/upgrade"
 )
 
-const (
-	bgMessages = 5
-)
-
-func TestSkipAtBackgroundVerification(t *testing.T) {
-	config, buf := newConfig(t)
-	skipMsg := "It is expected to be skipped"
-	expectedTexts := []string{
-		upgradeTestRunning,
-		"DEBUG\tFinished \"ShouldBeSkipped\"",
-		"DEBUG\tFinished \"ShouldNotBeSkipped\"",
-		upgradeTestSuccess,
-		"INFO\tSetup 1",
-		"INFO\tSetup 2",
-		"INFO\tVerify 2",
-	}
-	s := upgrade.Suite{
-		Tests: upgrade.Tests{
-			Continual: []upgrade.BackgroundOperation{
-				upgrade.NewBackgroundVerification("ShouldBeSkipped",
-					func(c upgrade.Context) {
-						c.Log.Info("Setup 1")
-					},
-					func(c upgrade.Context) {
-						c.T.Skip(skipMsg)
-						c.Log.Info("Verify 1")
-					},
-				),
-				upgrade.NewBackgroundVerification("ShouldNotBeSkipped",
-					func(c upgrade.Context) {
-						c.Log.Info("Setup 2")
-					},
-					func(c upgrade.Context) {
-						c.Log.Info("Verify 2")
-					},
-				),
-			},
-		},
-	}
-	s.Execute(config)
-
-	assert := assertions{tb: t}
-
-	out := buf.String()
-	assert.textNotContains(out, texts{elms: []string{
-		"INFO\tVerify 1",
-	}})
-	assert.textContains(out, texts{elms: expectedTexts})
-}
-
-func verifyBackgroundLogs(t *testing.T, logs string) {
-	t.Helper()
-	for _, line := range strings.Split(logs, "\n") {
-		if (strings.Contains(line, "BeforeVerify") ||
-			strings.Contains(line, "InVerify")) &&
-			!strings.Contains(line, "⏳") {
-			t.Fatalf("Message was not logged by background logger: %q", line)
-		}
-	}
-}
-
 func TestFailAtBackgroundVerification(t *testing.T) {
-	doneCh := make(chan struct{})
-	beforeVerifyCh := make(chan struct{})
-	inVerifyCh := make(chan struct{})
 	const failingVerification = "FailAtVerification"
 	expectedTexts := []string{
 		upgradeTestRunning,
@@ -104,29 +39,9 @@ func TestFailAtBackgroundVerification(t *testing.T) {
 					// Setup
 					func(c upgrade.Context) {
 						c.Log.Info("Setup 1")
-						go func() {
-							// Log messages before Verify phase.
-							for i := 0; i < bgMessages; i++ {
-								msg := fmt.Sprintf("BeforeVerify %d", i)
-								c.Log.Info(msg)
-								expectedTexts = append(expectedTexts, msg)
-							}
-							close(beforeVerifyCh)
-							<-inVerifyCh
-							// Log messages while Verify phase is in progress.
-							for i := 0; i < bgMessages; i++ {
-								msg := fmt.Sprintf("InVerify %d", i)
-								c.Log.Info(msg)
-								expectedTexts = append(expectedTexts, msg)
-							}
-							close(doneCh)
-						}()
 					},
 					// Verify
 					func(c upgrade.Context) {
-						<-beforeVerifyCh
-						close(inVerifyCh)
-						<-doneCh
 						c.Log.Info("Verify 1")
 						c.T.Fatal(failureTestingMessage)
 						c.Log.Info("Verify 2")
@@ -164,5 +79,4 @@ func TestFailAtBackgroundVerification(t *testing.T) {
 			fmt.Sprintf("--- FAIL: %s/VerifyContinualTests/%s", t.Name(), failingVerification),
 		},
 	})
-	verifyBackgroundLogs(t, out)
 }
