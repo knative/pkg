4af016b01b58cc6d4f91d9a5165439028ea90578
Martin Gencur <mgencur@redhat.com>
2023-01-04 14:44:26 +0100

    Execute OnWait at least once

--- test/upgrade/functions.go
+++ test/upgrade/functions.go
@@ -93,15 +93,15 @@ func NewBackgroundOperation(name string, setup func(c Context),
 // failures.
 func WaitForStopEvent(bc BackgroundContext, w WaitForStopEventConfiguration) {
 	for {
+		w.OnWait(bc, w)
 		select {
 		case <-bc.Stop:
 			bc.Log.Debugf("%s received a stop event", w.Name)
 			w.OnStop()
 			return
 		default:
-			w.OnWait(bc, w)
+			time.Sleep(w.WaitTime)
 		}
-		time.Sleep(w.WaitTime)
 	}
 }
 
