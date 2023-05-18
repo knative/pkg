d76082f57f9a2049d7c5adb89ebbe8e4d883eaf4
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-05-18 19:35:57 +0530

    added test
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- apis/duck/v1beta1/destination_test.go
+++ apis/duck/v1beta1/destination_test.go
@@ -31,6 +31,34 @@ const (
 	name       = "a-name"
 )
 
+var (
+	testCert = `-----BEGIN CERTIFICATE-----
+MIIDmjCCAoKgAwIBAgIUYzA4bTMXevuk3pl2Mn8hpCYL2C0wDQYJKoZIhvcNAQEL
+BQAwLzELMAkGA1UEBhMCVVMxIDAeBgNVBAMMF0tuYXRpdmUtRXhhbXBsZS1Sb290
+LUNBMB4XDTIzMDQwNTEzMTUyNFoXDTI2MDEyMzEzMTUyNFowbTELMAkGA1UEBhMC
+VVMxEjAQBgNVBAgMCVlvdXJTdGF0ZTERMA8GA1UEBwwIWW91ckNpdHkxHTAbBgNV
+BAoMFEV4YW1wbGUtQ2VydGlmaWNhdGVzMRgwFgYDVQQDDA9sb2NhbGhvc3QubG9j
+YWwwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC5teo+En6U5nhqn7Sc
+uanqswUmPlgs9j/8l21Rhb4T+ezlYKGQGhbJyFFMuiCE1Rjn8bpCwi7Nnv12Y2nz
+FhEv2Jx0yL3Tqx0Q593myqKDq7326EtbO7wmDT0XD03twH5i9XZ0L0ihPWn1mjUy
+WxhnHhoFpXrsnQECJorZY6aTrFbGVYelIaj5AriwiqyL0fET8pueI2GwLjgWHFSH
+X8XsGAlcLUhkQG0Z+VO9usy4M1Wpt+cL6cnTiQ+sRmZ6uvaj8fKOT1Slk/oUeAi4
+WqFkChGzGzLik0QrhKGTdw3uUvI1F2sdQj0GYzXaWqRz+tP9qnXdzk1GrszKKSlm
+WBTLAgMBAAGjcDBuMB8GA1UdIwQYMBaAFJJcCftus4vj98N0zQQautsjEu82MAkG
+A1UdEwQCMAAwCwYDVR0PBAQDAgTwMBQGA1UdEQQNMAuCCWxvY2FsaG9zdDAdBgNV
+HQ4EFgQUnu/3vqA3VEzm128x/hLyZzR9JlgwDQYJKoZIhvcNAQELBQADggEBAFc+
+1cKt/CNjHXUsirgEhry2Mm96R6Yxuq//mP2+SEjdab+FaXPZkjHx118u3PPX5uTh
+gTT7rMfka6J5xzzQNqJbRMgNpdEFH1bbc11aYuhi0khOAe0cpQDtktyuDJQMMv3/
+3wu6rLr6fmENo0gdcyUY9EiYrglWGtdXhlo4ySRY8UZkUScG2upvyOhHTxVCAjhP
+efbMkNjmDuZOMK+wqanqr5YV6zMPzkQK7DspfRgasMAQmugQu7r2MZpXg8Ilhro1
+s/wImGnMVk5RzpBVrq2VB9SkX/ThTVYEC/Sd9BQM364MCR+TA1l8/ptaLFLuwyw8
+O2dgzikq8iSy1BlRsVw=
+-----END CERTIFICATE-----
+`
+
+	invaidCert = "certificate"
+)
+
 func TestValidateDestination(t *testing.T) {
 	ctx := context.Background()
 
@@ -186,6 +214,25 @@ func TestValidateDestination(t *testing.T) {
 				DeprecatedAPIVersion: apiVersion,
 				DeprecatedName:       name,
 			},
+		},
+		"valid, CACert is valid": {
+			dest: &Destination{
+				URI: &apis.URL{
+					Path: "/handler",
+				},
+				Ref:     &validRef,
+				CACerts: &testCert,
+			},
+		},
+		"invalid,CACert is invalid": {
+			dest: &Destination{
+				URI: &apis.URL{
+					Path: "/handler",
+				},
+				Ref:     &validRef,
+				CACerts: &invaidCert,
+			},
+			want: "invalid value: CA Cert provided is invalid: caCert",
 		},
 	}
 
