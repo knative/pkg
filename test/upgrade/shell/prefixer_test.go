9051a454f03f7b5ea31fbbcce9b275af6d184a89
Martin Gencur <mgencur@redhat.com>
2023-10-12 12:44:54 +0200

    Migrate knative.dev/hack/shell to knative.dev/pkg/test/shell (#2856)
    * Move shell package from knative.dev/hack under test/shell
    
    * Provide Streams for shell executor that writes to t.Log
    
    * Remove temporary comment
    
    * Fix style
    
    * Fix lint
    
    * Do not fail when writing to stderr
    
    * use own interface TestingT
    
    * Fix arguments for Logf
    
    * Fix comments
    
    * NewExecutor function requires TestingT and ProjectLocation
    
    * Move pkg/test/shell under pkg/test/upgrade/shell
    
    * ProjectLocation is always passed
    
    It is now a required argument for NewExecutor function
    
    * Remove redundant file

---
+++ test/upgrade/shell/prefixer_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package shell_test
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+
+	"knative.dev/pkg/test/upgrade/shell"
+)
+
+func TestNewPrefixer(t *testing.T) {
+	assert := assertions{t: t}
+	var lineno int64
+	tests := []struct {
+		name   string
+		prefix func() string
+		want   string
+	}{{
+		"static",
+		func() string {
+			return "[prefix] "
+		},
+		`[prefix] test string 1
+[prefix] test string 2
+`,
+	}, {
+		"empty",
+		func() string {
+			return ""
+		},
+		`test string 1
+test string 2
+`,
+	}, {
+		"dynamic",
+		func() string {
+			lineno++
+			return strconv.FormatInt(lineno, 10) + ") "
+		},
+		`1) test string 1
+2) test string 2
+`,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			writer := &bytes.Buffer{}
+			wr := shell.NewPrefixer(writer, tt.prefix)
+			_, err := wr.Write([]byte("test string 1\ntest string 2\n"))
+			assert.NoError(err)
+			got := writer.String()
+			assert.Equal(tt.want, got)
+		})
+	}
+}
