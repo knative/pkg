545d0358bc6a7723a8c74aa6f6c5c0bf6921d130
Jawed khelil <jkhelil@redhat.com>
2026-02-24 08:39:45 +0100

    Add TLS support to Prometheus metrics server
        - Add WithTLSConfig() and WithTLSCertFiles() server options
        - Support METRICS_TLS_CERT and METRICS_TLS_KEY env vars

--- observability/metrics/prometheus/server.go
+++ observability/metrics/prometheus/server.go
@@ -18,6 +18,7 @@ package prometheus
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"net"
 	"net/http"
@@ -36,12 +37,16 @@ const (
 	defaultPrometheusHost            = "" // IPv4 and IPv6
 	prometheusPortEnvName            = "METRICS_PROMETHEUS_PORT"
 	prometheusHostEnvName            = "METRICS_PROMETHEUS_HOST"
+	prometheusTLSCertEnvName         = "METRICS_TLS_CERT"
+	prometheusTLSKeyEnvName          = "METRICS_TLS_KEY"
 )
 
 type ServerOption func(*options)
 
 type Server struct {
-	http *http.Server
+	http     *http.Server
+	certFile string
+	keyFile  string
 }
 
 func NewServer(opts ...ServerOption) (*Server, error) {
@@ -56,6 +61,8 @@ func NewServer(opts ...ServerOption) (*Server, error) {
 
 	envOverride(&o.host, prometheusHostEnvName)
 	envOverride(&o.port, prometheusPortEnvName)
+	envOverride(&o.certFile, prometheusTLSCertEnvName)
+	envOverride(&o.keyFile, prometheusTLSKeyEnvName)
 
 	if err := validate(&o); err != nil {
 		return nil, err
@@ -68,16 +75,21 @@ func NewServer(opts ...ServerOption) (*Server, error) {
 
 	return &Server{
 		http: &http.Server{
-			Addr:    addr,
-			Handler: mux,
-			// https://medium.com/a-journey-with-go/go-understand-and-mitigate-slowloris-attack-711c1b1403f6
+			Addr:              addr,
+			Handler:           mux,
+			TLSConfig:         o.tlsConfig,
 			ReadHeaderTimeout: 5 * time.Second,
 		},
+		certFile: o.certFile,
+		keyFile:  o.keyFile,
 	}, nil
 }
 
-func (s *Server) ListenAndServe() {
-	s.http.ListenAndServe()
+func (s *Server) ListenAndServe() error {
+	if s.http.TLSConfig != nil || (s.certFile != "" && s.keyFile != "") {
+		return s.http.ListenAndServeTLS(s.certFile, s.keyFile)
+	}
+	return s.http.ListenAndServe()
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
@@ -85,8 +97,11 @@ func (s *Server) Shutdown(ctx context.Context) error {
 }
 
 type options struct {
-	host string
-	port string
+	host      string
+	port      string
+	tlsConfig *tls.Config
+	certFile  string
+	keyFile   string
 }
 
 func WithHost(host string) ServerOption {
@@ -98,6 +113,22 @@ func WithHost(host string) ServerOption {
 func WithPort(port string) ServerOption {
 	return func(o *options) {
 		o.port = port
+	}
+}
+
+// WithTLSConfig configures the server to use the provided TLS configuration.
+// This allows programmatic control over TLS settings like MinVersion, CipherSuites, etc.
+func WithTLSConfig(cfg *tls.Config) ServerOption {
+	return func(o *options) {
+		o.tlsConfig = cfg
+	}
+}
+
+// WithTLSCertFiles configures the server to use TLS with the provided certificate and key files.
+func WithTLSCertFiles(certFile, keyFile string) ServerOption {
+	return func(o *options) {
+		o.certFile = certFile
+		o.keyFile = keyFile
 	}
 }
 
@@ -121,4 +152,22 @@ func envOverride(target *string, envName string) {
 	if val != "" {
 		*target = val
 	}
+}
+
+type tlsConfigKey struct{}
+
+// ContextWithTLSConfig adds a TLS configuration to the context.
+// This allows programmatic configuration of TLS settings like MinVersion, CipherSuites, ClientAuth, etc.
+// when using frameworks like sharedmain where direct NewServer() options aren't accessible.
+func ContextWithTLSConfig(ctx context.Context, cfg *tls.Config) context.Context {
+	return context.WithValue(ctx, tlsConfigKey{}, cfg)
+}
+
+// TLSConfigFromContext retrieves the TLS configuration from the context.
+// Returns nil if no TLS configuration was set.
+func TLSConfigFromContext(ctx context.Context) *tls.Config {
+	if cfg, ok := ctx.Value(tlsConfigKey{}).(*tls.Config); ok {
+		return cfg
+	}
+	return nil
 }
